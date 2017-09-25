7dfb8dbfc894af3f81f948fbf3445d5a9cf2b3d9
amelow <amelielowe96@gmail.com>
2017-09-25 12:04:01 +0200

    Weird output
    ava.util.Scanner[delimiters=\p{javaWhitespace}+][position=4][match
    valid=true][need input=false][source closed=false][skipped=false][group
    separator=\ ][decimal separator=\,][positive prefix=][negative
    prefix=\Q-\E][positive suffix=][negative suffix=][NaN
    string=\Q�\E][infinity string=\Q∞\E]

---
+++ src/Lab1/Citat.java
@@ -0,0 +1,16 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Citat {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Skriv ett citat: ");
+
+		sc.nextLine();
+		System.out.println(" Citatet: " + sc);
+		sc.close();
+
+	}
+
+}
