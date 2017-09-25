8642acf74288171e967c6751fe1109b8028a6b22
amelow <amelielowe96@gmail.com>
2017-09-25 11:56:41 +0200

    Print

---
+++ src/Lab1/Print.java
@@ -0,0 +1,16 @@
+package Lab1;
+
+public class Print {
+
+	public static void main(String[] args) {
+		System.out.println("Kunskap är makt!");
+		System.out.println("Kunskap ");
+		System.out.println("är");
+		System.out.println("makt!");
+		System.out.println("==================");
+		System.out.println("|Kunskap är makt!|");
+		System.out.println("==================");
+
+	}
+
+}
