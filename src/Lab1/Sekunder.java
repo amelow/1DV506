5d1f64d6a361e617d17f0e2ae0a0c0443e4d0ed7
amelow <amelielowe96@gmail.com>
2017-10-08 18:21:30 +0200

    Program- Done

--- src/Lab1/Sekunder.java
+++ src/Lab1/Sekunder.java
@@ -9,12 +9,22 @@ and then calculates the corresponding time in seconds.
 public class Sekunder {
 
 	public static void main(String[] args) {
+		final int SECONDS_PER_HOUR = 3600;
+		final int SECONDS_PER_MINUTE = 60;
+		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("How many Hours?");
+		System.out.println("How many Hours? ");
 		int hours = sc.nextInt();
-		System.out.println("How many Minuits?");
+		
+		System.out.println("How many Minuits? ");
 		int minuits = sc.nextInt();
-
-	}
+	
+		System.out.println("How many Seconds? ");
+		int seconds = sc.nextInt();
+		sc.close();
+		
+		int total = (hours*SECONDS_PER_HOUR)+(minuits*SECONDS_PER_MINUTE)+seconds;
+		System.out.println("Here are your total in seconds: "+ total);
+	}	
 
 }
