0f7269a6b8ce4a82001fc340c293faacfac8093c
amelow <amelielowe96@gmail.com>
2017-10-08 18:06:28 +0200

    Added printouts

--- src/Lab1/Sekunder.java
+++ src/Lab1/Sekunder.java
@@ -2,12 +2,18 @@ package Lab1;
 
 import java.util.Scanner;
 
+/*Number of seconds:
+Write a program called Sekunder.java which reads three integers (hours, minutes, seconds)
+and then calculates the corresponding time in seconds.
+*/
 public class Sekunder {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
+		System.out.println("How many Hours?");
+		int hours = sc.nextInt();
+		System.out.println("How many Minuits?");
+		int minuits = sc.nextInt();
 
 	}
 
