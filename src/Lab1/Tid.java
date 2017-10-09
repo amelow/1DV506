16153fb7e12258d7e5b2ddbe0e208c87c5f66c0c
amelow <amelielowe96@gmail.com>
2017-10-09 14:22:21 +0200

    Time converter

---
+++ src/Lab1/Tid.java
@@ -0,0 +1,26 @@
+package Lab1;
+
+import java.util.Scanner;
+/*
+ * Type a program Time.java which reads a number of seconds (an integer) and then prints the same time in terms of hours, minutes and seconds.
+ */
+
+public class Tid {
+
+	public static void main(String[] args) {
+		final int SECONDS_PER_HOUR = 3600;
+		final int SECONDS_PER_MINUTE = 60;
+		System.out.println("This is a time converter ");
+		System.out.println("===============");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter the Seconds: ");
+		int allSeconds = sc.nextInt();
+		
+		int hours = allSeconds/(SECONDS_PER_HOUR );
+		int rest = allSeconds- (SECONDS_PER_HOUR* hours);
+	
+		
+
+	}
+
+}
