1d60a3b7850bc54d245403c0e2c2fd5d135f4e60
amelow <amelielowe96@gmail.com>
2017-10-09 15:53:00 +0200

    Add total,first, second and third variable

--- src/Lab1/Sum.java
+++ src/Lab1/Sum.java
@@ -1,15 +1,25 @@
 package Lab1;
 
+import java.util.Scanner;
+
 public class Sum {
 	/*
 	 * 
-	 * Write a program  where the user is asked to enter
-	 * a three-digit number. The program should then generate the sum of the
-	 * numbers the integer consists of and print the sum.
+	 * Write a program where the user is asked to enter a three-digit number.
+	 * The program should then generate the sum of the numbers the integer
+	 * consists of and print the sum.
 	 */
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("");
+		int total = sc.nextInt();
+		int first = (total/100);
+		int second= (total/(10%10));
+		int third = (total%10);
+		
+		
 	}
 
 }
