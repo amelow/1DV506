f29556b6adb78947e1c53df0e9361814e72ac3d0
amelow <amelielowe96@gmail.com>
2017-10-09 14:37:44 +0200

    New program
    Sum of a three- digit number

---
+++ src/Lab1/Sum.java
@@ -0,0 +1,15 @@
+package Lab1;
+
+public class Sum {
+	/*
+	 * 
+	 * Write a program  where the user is asked to enter
+	 * a three-digit number. The program should then generate the sum of the
+	 * numbers the integer consists of and print the sum.
+	 */
+
+	public static void main(String[] args) {
+
+	}
+
+}
