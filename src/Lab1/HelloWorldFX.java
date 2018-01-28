ab5343b3bedb5f9cf53ec1b092e020b9b229f03a
amelow <amelielowe96@gmail.com>
2018-01-28 17:12:51 +0100

    Easy javafx program

---
+++ src/Lab1/HelloWorldFX.java
@@ -0,0 +1,36 @@
+package Lab1;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorldFX extends Application {
+
+	public static void main(String[] args) {
+		launch();
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Button button = new Button();
+		primaryStage.setTitle("Hello Wold Test");
+		button.setText("press");
+		button.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Hello world");
+			}
+		});
+		StackPane root = new StackPane();
+		root.getChildren().add(button);
+		primaryStage.setScene(new Scene(root, 300, 250));
+		primaryStage.show();
+	}
+
+}
