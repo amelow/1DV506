28615dc4aff67780fbe8a1c84ac68d48d5496fc4
amelow <amelielowe96@gmail.com>
2018-02-04 18:58:00 +0100

    Done

--- src/Lab1/remove99.java
+++ src/Lab1/remove99.java
@@ -1,14 +1,41 @@
 package Lab1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class remove99 {
 	/*
-	 * Input integers, if number 99 remove and put in the front
-	 * use at least one method as well
+	 * Input integers, if number 99 remove and put in the front use at least one
+	 * method as well
 	 */
 
 	public static void main(String[] args) {
-		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter Integers, one of them has to be the number 99! Stop with non-numeric input.");
+		int number = 0;
+		while (sc.hasNextInt()) {
+			number = sc.nextInt();
+			if (number >= 0) {
+				list.add(number);
 
+			}
+
+		}
+		System.out.println(list);
+		System.out.println(listCheck(list));
+
+	}
+
+	private static ArrayList<Integer> listCheck(ArrayList<Integer> list) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) == 99) {
+				list.remove(i);
+				list.add(0, 99);
+			}
+
+		}
+		return list;
 	}
 
 }
