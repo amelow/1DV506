111dc5108c544eaef187d0649e738754d76b16ad
amelow <amelielowe96@gmail.com>
2018-02-04 18:28:08 +0100

    new program remove99

--- src/Lab1/HelloWorldFX.java
+++ src/Lab1/HelloWorldFX.java
@@ -1,10 +1,10 @@
 package Lab1;
 
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.StackPane;
@@ -12,27 +12,31 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class HelloWorldFX extends Application {
+	DropShadow shadow = new DropShadow();
+	final Button button = new Button();
 
 	public static void main(String[] args) {
-		launch();
+		launch(args);
 
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Button button = new Button();
 		StackPane root = new StackPane();
+
 		primaryStage.setTitle("Hello Wold Test");
 		button.setText("press");
+		root.getChildren().add(button);
+		primaryStage.setScene(new Scene(root, 300, 250));
+		button.setStyle(" -fx-background-color:#64EE64;");
 
 		button.setOnAction(actionEvent -> {
 			root.setBackground(new Background(new BackgroundFill(Color.BLUE, null, null)));
 			System.out.println("Hello there :) ");
+			button.setEffect(new DropShadow());
+
 		});
 
-		root.getChildren().add(button);
-		primaryStage.setScene(new Scene(root, 300, 250));
-		button.setStyle(" -fx-background-color:#64EE64;");
 		root.setBackground(new Background(new BackgroundFill(Color.DARKCYAN, null, null)));
 		primaryStage.show();
 	}
---
+++ src/Lab1/remove99.java
@@ -0,0 +1,14 @@
+package Lab1;
+
+public class remove99 {
+	/*
+	 * Input integers, if number 99 remove and put in the front
+	 * use at least one method as well
+	 */
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
