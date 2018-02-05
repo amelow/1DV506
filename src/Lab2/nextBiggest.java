b4a188a39c1c723fc13d5e28fff2a1fac2bcb9ab
amelow <amelielowe96@gmail.com>
2018-02-05 15:05:24 +0100

    new program

---
+++ src/Lab2/nextBiggest.java
@@ -0,0 +1,16 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class nextBiggest {
+	/*
+	 * Write 10 integers, printout the next biggest one
+	 */
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int biggest = 0;
+		int nextBiggest = 0;
+	}
+
+}
