dcb280eeeedada4f27e2e827d4405b32b6fbd079
amelow <amelielowe96@gmail.com>
2018-02-11 13:50:45 +0100

    update

--- src/Lab2/nextBiggest.java
+++ src/Lab2/nextBiggest.java
@@ -8,9 +8,27 @@ public class nextBiggest {
 	 */
 
 	public static void main(String[] args) {
+		System.out.println("Enter 10 integers:");
+		int biggest, nextBiggest, numbers;
+		biggest = Integer.MIN_VALUE;
+		nextBiggest = Integer.MIN_VALUE;
 		Scanner sc = new Scanner(System.in);
-		int biggest = 0;
-		int nextBiggest = 0;
+		numbers = sc.nextInt();
+		
+		
+		for (int i = 1; i <= numbers; i++) {
+			int number = sc.nextInt();
+			if (number > biggest) {
+				nextBiggest = biggest;
+				biggest = number;
+				
+			} else if (nextBiggest > number) {
+				nextBiggest = number;
+
+			}
+		}
+		System.out.println("Second biggest integer is: " + nextBiggest);
+		sc.close();
 	}
 
 }
